c84b4d91e535773ea2dd8822e73718202eee6fca
Jordan Zangara <jordanozang@protonmail.com>
2024-10-22 08:50:46 -0400

    nsync dependency was removed

--- onnxruntime/portfile.cmake
+++ onnxruntime/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
 	REPO jordanozang/onnxruntime
 	OUT_SOURCE_PATH SOURCE_PATH
-	REF 77f7188fe83de43a73a85f54ff6aab88753290da 
-	SHA512 3e847f67a13d728d4d0a2f7e82181fc4f5d9a29f965d6b13e0c66ff62d58dc1d08c8aca3551abfccc46c6a752e6e4b9b86e95feb4e96f15878ed35f5a1ac95b3 
+	REF b3aa2b75a7ceaf333fa09b3a4881b484f024fc16 
+	SHA512 bfc15501587d6155906c2880bde45b87688ed9aac745650c1630959231e0bb8d459d61bb13239400b91cf8681e1616f95ca18e582e01ff08a63bb01967fc0b72 
  )
 
 vcpkg_cmake_configure(
@@ -19,4 +19,3 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/LICENSE)
-
