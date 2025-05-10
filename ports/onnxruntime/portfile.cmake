f304fcbbf74c5b82e84d87108bdde4b85c2e5ae6
Jordan Zangara <jordanozang@protonmail.com>
2025-05-10 12:31:53 -0400

    new changes

--- ports/onnxruntime/portfile.cmake
+++ ports/onnxruntime/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
 	REPO jordanozang/onnxruntime
 	OUT_SOURCE_PATH SOURCE_PATH
-	REF b3aa2b75a7ceaf333fa09b3a4881b484f024fc16 
-	SHA512 bfc15501587d6155906c2880bde45b87688ed9aac745650c1630959231e0bb8d459d61bb13239400b91cf8681e1616f95ca18e582e01ff08a63bb01967fc0b72 
+	REF 20f7f58d7744bbe0d303c22caa4efd148cc86006 
+	SHA512 3f2b807a414161bfd39dedb7dbc4a86c12620b6032f957a903be887e47be504707a8509a58ed4ec0747c88b15a561282385a6713535fa3fc56f9e12ca67df9c5
  )
 
 vcpkg_cmake_configure(
@@ -10,6 +10,7 @@ vcpkg_cmake_configure(
 	OPTIONS
 	--compile-no-warning-as-error
 	-Donnxruntime_USE_EIGEN_PACKAGE=ON
+	-Donnxruntime_USE_VCPKG=ON
 )
 
 vcpkg_cmake_install()
